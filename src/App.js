805619fb0b5c26cdd05edc17b0128733193b54fe
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-05-07 00:09:58 +0200

    Add Header and Content components to App structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import LanguageProvider from "./contexts/LanguageProvider";
+import Header from "./components/Header";
+import Content from "./components/Content";
 
 function App() {
   return (
     <LanguageProvider>
       <div className="App">
-        <header className="App-header ">Language Context App</header>
+        <Header />
+        <Content />
       </div>
     </LanguageProvider>
   );
---
+++ src/components/Content.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <main>
+      <h2>Language Context App</h2>
+      <p>
+        This is a simple app to demonstrate the use of context API in React.
+      </p>
+      <p>Change the language using the buttons in the header.</p>
+    </main>
+  );
+};
+
+export default Content;
