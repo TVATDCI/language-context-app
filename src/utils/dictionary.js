9b95c514d3f1fc879cfa0af7dc18b491ba5f7321
TVATDCI <tvatdci@gmail.com>
2025-05-07 11:27:55 +0200

    init dictionary

---
+++ src/utils/dictionary.js
@@ -0,0 +1,44 @@
+const dictionary = {
+  hello: {
+    en: "Hello",
+    de: "Hallo",
+    tr: "Merhaba",
+    ir: "سلام",
+    fr: "Bonjour",
+    sp: "Hola",
+    du: "Hallo",
+    th: "สวัสดี",
+  },
+  world: {
+    en: "World",
+    de: "Welt",
+    tr: "Dünya",
+    ir: "دنیا",
+    fr: "le monde",
+    sp: "Mundo",
+    du: "Wereld",
+    th: "โลก",
+  },
+  good: {
+    en: "Good",
+    de: "Gut",
+    tr: "İyi",
+    ir: "خوب",
+    fr: "Bon",
+    sp: "Bueno",
+    du: "Goed",
+    th: "ดี",
+  },
+  morning: {
+    en: "Morning",
+    de: "Morgen",
+    tr: "Sabah",
+    ir: "صبح",
+    fr: "Matin",
+    sp: "Mañana",
+    du: "Ochtend",
+    th: "เช้า",
+  },
+};
+
+export default dictionary;
